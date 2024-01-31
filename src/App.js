a27d87b9d5340d9d2c956284c4884f02171bee30
karthik156kk <karthik582tn@gmail.com>
2024-01-31 09:02:30 +0530

    chapter 1: App and JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+//can edit the functional components
 function App() {
+  const name = 'karthik';
+  const handleNameChange = () => {
+    const names = ['karthik', 'arjun', 'kausik'];
+    const int = Math.floor(Math.random()*3);
+    return names[int];
+  }
   return (
+    // return JsX = Javascript + XML
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to see changes what I change.
         </p>
         <a
           className="App-link"
@@ -17,9 +24,13 @@ function App() {
         >
           Learn React
         </a>
+        <p>{true}</p> 
+        <p>hello {name}</p>
+        <p>{handleNameChange()}</p>
       </header>
     </div>
   );
 }
+//cannot render boolean and objects
 
 export default App;
