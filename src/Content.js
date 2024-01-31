f36980c0c968131f23d3daace377a748df411017
karthik156kk <karthik582tn@gmail.com>
2024-01-31 11:24:36 +0530

    chapter 6: useState Hook

--- src/Content.js
+++ src/Content.js
@@ -1,16 +1,19 @@
+import {useState} from 'react';
 const Content = () => {
-    const name = 'karthik';
+    const [name, setName] = useState('karthik');
+    //gotchas
+    const [count, setCount] = useState(0);
     const handleNameChange = () => {
       const names = ['karthik', 'arjun', 'kausik'];
       const int = Math.floor(Math.random()*3);
-      return names[int];
+      setName(names[int]);
     }
-    const handleClick = () => {
-        console.log("you clicked it");
-    }
-    //anonymous function is not called until its clicked. so inner function is invoked immediately only after clicking
-    const handleClick2 = (name) => {
-        console.log("you clicked and my name is: ", name);
+
+    const handleClick2 = () => {
+        //count is actually the value of count when brought into the function
+        setCount(count+1);
+        setCount(count+1); //does not add +1 to count as count is simply count+1 twice without actually changing in every step
+        console.log(count);
     }
     const handleClick3 = (e) => {
         console.log("event target: ", e.target);
@@ -18,10 +21,10 @@ const Content = () => {
     return (
         <main>
             <p onDoubleClick={(e)=>handleClick3(e)}>
-                {name} , Hello {handleNameChange()}!
+                {name} , Hello {name}!
             </p>
-            <button onClick={handleClick}>Please click</button>
-            <button onClick={()=>handleClick2(name)}>also click me</button>
+            <button onClick={handleNameChange}>Please click to change name</button>
+            <button onClick={()=>handleClick2()}>also click me</button>
             <button onClick={(e)=>handleClick3(e)}>finally click me</button>
         </main>
     )
